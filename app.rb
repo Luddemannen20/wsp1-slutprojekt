51e3e470ecfe05ffb83175759b9942c76b1cef3f
Luddemannen20 <07ludlar@gmail.com>
2026-03-14 17:33:06 +0100

    2026-03-14
    Färdig med log in samt sign up funktionen, jag hade repositoryt på classroom till mycket stor hjälp samt kollade på youtube för att lyckas, finns vissa detaljer som eventuellt kan fin slipas men känner mig nöjd för tillfället

--- app.rb
+++ app.rb
@@ -4,15 +4,14 @@ require 'sinatra'
 require 'securerandom'
 require 'bcrypt'
 require 'sqlite3'
-require 'bcrypt'
-require '../config'
+require_relative 'config'
 
 class App < Sinatra::Base
   
   setup_development_features(self)
 
   configure do
-    enable :session
+    enable :sessions
     set :session_secret, SecureRandom.hex(64)
   end
 
@@ -25,7 +24,7 @@ class App < Sinatra::Base
 
     before do
       if session[:user_id]
-        @current_user = db.execute("SELECT * FROM users WHERE id = ?", session[:user_id]).first
+        @current_user = db.execute("SELECT * FROM users WHERE id = ?", [session[:user_id]]).first
         ap @current_user
       end
     end
@@ -40,12 +39,12 @@ class App < Sinatra::Base
     end
 
     get '/recipes/new' do
-      redirect 'access_denied' unless session[:user_id]
+      redirect '/access_denied' unless session[:user_id]
       erb(:"recipes/new")
     end
 
     post '/recipes' do
-      redirect '/acess_denied' unless session[:user_id]
+      redirect '/access_denied' unless session[:user_id]
 
       recipe_name = params['recipe_name']
       recipe_time = params['recipe_time']
@@ -71,7 +70,7 @@ class App < Sinatra::Base
     end
 
     post '/recipes/:id/update' do | id |
-      redirect '/acess_denied' unless session[:user_id]
+      redirect '/access_denied' unless session[:user_id]
       
       recipe_name = params['recipe_name']
       recipe_time = params['recipe_time']
@@ -87,8 +86,78 @@ class App < Sinatra::Base
     end
 
     post '/login' do
-      
+      request_username = params[:username]
+      request_plain_password = params[:password]
+
+      user = db.execute("SELECT * FROM users WHERE username = ?", [request_username]).first
+
+      unless user
+        ap "/login : Invalid username."
+        status 401
+        redirect '/access_denied'
+      end
+
+      db_id = user['id'].to_i
+      db_password_hashed = user['password'].to_s
+
+      bcrypt_db_password = BCrypt::Password.new(db_password_hashed)
+
+      if bcrypt_db_password == request_plain_password
+        ap "/login : Logged in -> redirecting to admin"
+        session[:user_id] = db_id
+        redirect '/admin'
+      else
+        ap "/login : Invalid password."
+        redirect '/access_denied'
+      end
     end
+
+    post '/logout' do
+      ap 'Logging out'
+      session.clear
+      redirect '/'
+    end
+
+    get '/admin' do 
+      if session[:user_id]
+        erb(:"admin/index")
+      else
+        ap "/admin : Access denied."
+        redirect '/access_denied'
+      end
+    end
+
+    get '/access_denied' do
+      erb(:access_denied)
+    end
+
+    get '/users/new' do
+      erb(:"users/new")
+    end
+
+    post '/users' do
+      username = params[:username]
+      password = params[:password]
+
+      if username.to_s.strip.empty? || password.to_s.strip.empty?
+        @error = "Fyll i användarnamn och lösenord!"
+        return erb(:"users/new")
+      end
+
+      existing_user = db.execute("SELECT * FROM users WHERE username = ?", [username]).first
+
+      if existing_user
+        @error = "Användarnamnet finns redan!"
+        return erb(:"users/new")
+      end
+
+      hashed_password = BCrypt::Password.create(password)
+
+      db.execute("INSERT INTO users (username, password) VALUES (?, ?)", [username, hashed_password])
+      redirect '/login'
+    end
+  end
+
 
 
     
